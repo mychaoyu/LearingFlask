5cce85157382996727c13adfb249f2ca944932a4
yuchaoyu <42341238+yuchaoyu@users.noreply.github.com>
2019-11-06 16:32:04 +0800

    重构代码

--- BasicProject/runserver.py
+++ BasicProject/runserver.py
@@ -3,17 +3,11 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask
-app = Flask(__name__)
-
+from HelloFlask import app
+import os
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
 
-
-@app.route('/')
-def hello():
-    """Renders a sample page."""
-    return "Hello World!"
 
 if __name__ == '__main__':
     import os
