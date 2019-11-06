c2d1e5942353d0b48e4d479de8f4715e5118b46e
yuchaoyu <42341238+yuchaoyu@users.noreply.github.com>
2019-11-06 19:42:09 +0800

    添加页面

--- BasicProject/HelloFlask/views.py
+++ BasicProject/HelloFlask/views.py
@@ -9,9 +9,18 @@ def home():
 	formatted_now=now.strftime("%A,%d %B,%Y at %X")
 
 	return render_template(
-		"/time/index.html",
+		"index.html",
 		title='Hello Flask',
 		message="Hello Flask !",
-		content=" on "+formatted_now
+		content=" on "+formatted_now)
 
-		)
+@app.route('/api/data')
+def get_data():
+  return app.send_static_file('data.json')
+
+@app.route('/about')
+def about():
+    return render_template(
+        "about.html",
+        title = "About HelloFlask",
+        content = "Example app page for Flask.")
